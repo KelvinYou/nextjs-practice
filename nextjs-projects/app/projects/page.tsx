8ef282409673438086f244d325c19f080f9a7a82
Kelvin <ykekelvin0220@gmail.com>
2023-05-17 10:47:12 +0800

    update

--- nextjs-projects/app/projects/page.tsx
+++ nextjs-projects/app/projects/page.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link'
 
 import { projects } from "@/data/data";
 import LinkButton from '@/components/projects/LinkButton';
+import ProjectCard from '@/components/projects/ProjectCard';
 
 const Projects = () => {
   return (
@@ -11,35 +12,7 @@ const Projects = () => {
         <div className="flex flex-wrap justify-center">
 
           {projects.map((project, index) => (
-            <div key={index + project.name} className="max-w-sm rounded overflow-hidden shadow-lg mx-4 my-4">
-              <div className="w-full" style={{ height: "200px" }}>
-                <img className="object-cover w-full h-full" src="/assets/images/demo.jpg" alt={project.name}/>
-              </div>
-              <div className="px-6 pt-4 pb-2">
-                {project.key_techs.map((tech, techIndex) => (
-                  <span key={techIndex + tech} className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                    {tech}
-                  </span>
-                ))}
-              </div>
-              
-              <div className="px-6 py-4">
-                <div className="font-bold text-xl mb-2">{project.name}</div>
-                <p className="text-gray-700 text-base">
-                  {project.description}
-                </p>
-              </div>
-
-              <div className="px-6 pb-4 flex flex-wrap justify-center">
-                {project.demo_url && 
-                  <LinkButton url={project.demo_url} name={"Demo"}/>
-                }
-                {project.github_url && 
-                  <LinkButton url={project.github_url} name={"Github"}/>
-                }
-              </div>
-              
-            </div>
+            <ProjectCard key={index} project={project} />
           ))}
         </div>
       </div>
--- nextjs-projects/data/data.ts
+++ nextjs-projects/data/data.ts
@@ -1,17 +1,5 @@
+import { Project } from "@/types/types";
 import React from "react";
-
-export type Category = "react" | "node" | "express" | "django" | "mongo";
-
-interface Project {
-  name: string;
-  description: string;
-  image_path: string;
-  demo_url?: string;
-  github_url?: string;
-  apk_url?: string;
-  category: Category[];
-  key_techs: string[];
-}
 
 export const projects: Project[] = [
   { 
