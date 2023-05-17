ae71e61f2175ffde5766bca2a420da301c4d5b15
Kelvin <ykekelvin0220@gmail.com>
2023-05-17 11:35:14 +0800

    changes

--- nextjs-projects/app/projects/page.tsx
+++ nextjs-projects/app/projects/page.tsx
@@ -1,8 +1,4 @@
-import Image from 'next/image'
-import Link from 'next/link'
-
 import { projects } from "@/data/data";
-import LinkButton from '@/components/projects/LinkButton';
 import ProjectCard from '@/components/projects/ProjectCard';
 
 const Projects = () => {
