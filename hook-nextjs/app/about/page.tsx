10ffdef48e5b5dcd66ea79f770c638014355aff3
Kelvin <ykekelvin0220@gmail.com>
2023-05-19 08:57:32 +0800

    update

---
+++ hook-nextjs/app/about/page.tsx
@@ -0,0 +1,16 @@
+
+
+export const metadata = {
+  title: "About Page",
+  description: "About page example",
+};
+
+const About = () => {
+  return (
+    <div>
+      About
+    </div>
+  )
+}
+
+export default About;
